3f7205a26e7fe3f7a44038426b6812616466b166
HolodTema <art.holodilov@gmail.com>
2026-04-06 17:40:25 -0400

    Task Wires was done.

--- wires/main.py
+++ wires/main.py
@@ -9,35 +9,44 @@ def load_images():
     for i in range(1, 7):
         image = np.load(f"./data/wires{i}.npy")
         images += [image]
-        # plt.imshow(image)
-        # plt.show()
     return images
 
 
 
 def main():
-    images = load_images()
+    print("Task wires")
+    print("PS. every wire has AT LEAST ONE PART")
 
+    images = load_images()
 
     for image_number, image in enumerate(images):
         print(f"Handling image {image_number}:")
 
         wire_sectors = list()
 
+        # crop image into wires-images
         crop_start_index = 0
         for i in range(1, image.shape[0]):
-            if np.all(image[i-1, :] != 0) and np.all(image[i, :] == 0):
-                wire_sectors += [(image[crop_start_index:i, :]).copy()]
+            if np.any(image[i-1, :] != 0) and np.all(image[i, :] == 0):
+                wire_sectors += [(image[crop_start_index:i+1, :]).copy()]
                 crop_start_index = i
-        
-        print(f"Amount wires: {len(wire_sectors)}")
-        
+                plt.imshow(wire_sectors[-1])
+                plt.show()
+
+        # use opening() to split wire into parts
+        # if wire is not broken, it has one part
         gap_kernel = np.ones((3, 1))
+        amount_wires = 0
         for i in range(len(wire_sectors)):
             wire_sectors[i] = opening(wire_sectors[i], gap_kernel)
             amount_wire_parts = label(wire_sectors[i]).max()
-            print(f"Wire {i}: amount parts: {amount_wire_parts}")
+            if amount_wire_parts != 0:
+                amount_wires += 1
+                print(f"Wire {i}: amount parts: {amount_wire_parts}")
+
+        print(f"Amount wires: {amount_wires}")
         print()
+
 
 
 
