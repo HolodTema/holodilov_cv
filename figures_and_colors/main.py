6c4274a733c389bb4b44b6da0197e96385a9f689
HolodTema <art.holodilov@gmail.com>
2026-04-26 01:25:02 -0400

    Solution using scikit-image is done

--- figures_and_colors/main.py
+++ figures_and_colors/main.py
@@ -4,16 +4,19 @@ from skimage.io import imread
 from skimage.measure import label, regionprops
 
 
-
+# get RGB tuple from the center of prop.image
 def get_centroid_rgb_color(src_image, prop):
     centroid_y, centroid_x = prop.centroid
     centroid_x = int(centroid_x)
     centroid_y = int(centroid_y)
-    print(centroid_x)
-    print(centroid_y)
-    print(src_image[centroid_y, centroid_x, :])
+    (red, green, blue) = src_image[centroid_y, centroid_x, :]
+    red = int(red)
+    green = int(green)
+    blue = int(blue)
+    return (red, green, blue)
 
 
+# if every horizontal line is fullfilled with 1 - we can say it is rectangle
 def is_rectangle(prop):
     for y in range(prop.image.shape[0]):
         if np.sum(prop.image[y, :]) != prop.image.shape[1]:
@@ -24,7 +27,6 @@ def is_rectangle(prop):
 
 def main():
     image = imread("./balls_and_rects.png")
-
     binary = image.sum(2) > 0
     labeled = label(binary)
     
@@ -37,16 +39,28 @@ def main():
     amount_rect = 0
     for prop in props:
         is_rect = is_rectangle(prop)
+        tuple_rgb = get_centroid_rgb_color(image, prop)
         if is_rect:
             amount_rect += 1
-        get_centroid_rgb_color(image, prop)
+            if tuple_rgb in dict_rect:
+                dict_rect[tuple_rgb] += 1
+            else:
+                dict_rect[tuple_rgb] = 1
+        else:
+            if tuple_rgb in dict_circle:
+                dict_circle[tuple_rgb] += 1
+            else:
+                dict_circle[tuple_rgb] = 1
 
 
     print(f"Amount rectangles = {amount_rect}")
     print(f"Amount circles = {amount_figures - amount_rect}")
-
-    plt.imshow(labeled)
-    plt.show()
+    print()
+    print("Rectangles by their colors (RGB):")
+    print(dict_rect)
+    print()
+    print("Circles by their colors (RGB):")
+    print(dict_circle)
 
 
 
