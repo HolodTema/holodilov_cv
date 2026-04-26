2039647ec96a1cb6f8e5f4e10562714a70acc051
HolodTema <art.holodilov@gmail.com>
2026-04-26 06:42:00 -0400

    Now autorun is working, but I need to find the way to determine game bbox

--- trex/main.py
+++ trex/main.py
@@ -1,8 +1,8 @@
 import time
 import pyautogui
-from PIL import ImageGrab
+import pyscreenshot
 import numpy as np
-from pynput.keyboard import Key, Listener, Controller
+import keyboard
 
 
 def check_obstacle():
@@ -11,57 +11,49 @@ def check_obstacle():
     BBOX_CHECK_BOTTOM_RIGHT_X = 350
     BBOX_CHECK_BOTTOM_RIGHT_Y = 380
 
-    screenshot = ImageGrab.grab(bbox=(BBOX_CHECK_TOP_LEFT_X, BBOX_CHECK_TOP_LEFT_Y, BBOX_CHECK_BOTTOM_RIGHT_X, BBOX_CHECK_BOTTOM_RIGHT_Y))
+    screenshot = pyscreenshot.grab(bbox=(BBOX_CHECK_TOP_LEFT_X, BBOX_CHECK_TOP_LEFT_Y, BBOX_CHECK_BOTTOM_RIGHT_X, BBOX_CHECK_BOTTOM_RIGHT_Y))
     array_screenshot = np.array(screenshot)
     dark_pixels = np.sum(array_screenshot < 100)
     return dark_pixels > 20
 
 
 
-def jump(keyboard_controller):
-    keyboard_controller.press(Key.space)
-    time.sleep(0.01)
-    keyboard_controller.release(Key.space)
+def jump():
+    keyboard.send("space")
 
 
     
 def main():
     print("T-REX autorunner (by python_hackershaa)")
-    print("F2 - start autorun")
-    print("F3 - stop autorun")
-    print("F4 - quit")
+    print("1 - start autorun")
+    print("2 - stop autorun")
+    print("3 - quit")
     print()
 
     is_running = False
 
-    keyboard_controller = Controller()
+    while True:
+        if keyboard.is_pressed('1'):
+            if not is_running:
+                is_running = True
+                print("autorun started")
 
-    def on_press(key):
-        nonlocal is_running
-        try:
-            if hasattr(key, "char") and key.char:
-                if key.char == "2":
-                    is_running = True
-                    print("Autorun started")
-                elif key.char == "3":
-                    is_running = False
-                    print("Autorun stopped")
-                elif key.char == "4":
-                    print("Quit")
-                    return False
-        except Exception as e:
-            print("error occured:", e)
-        return True
-
-
-    with Listener(on_press=on_press) as listener:
-        while listener.running:
+        if keyboard.is_pressed('2'):
             if is_running:
-                if check_obstacle():
-                    jump(keyboard_controller)
-                    time.sleep(0.05)
-                time.sleep(0.02)
-            
+                is_running = False
+                print("autorun stopped")
+
+        if keyboard.is_pressed('3'):
+            print("quit")
+            break
+
+        if is_running:
+            if check_obstacle():
+                jump()
+                time.sleep(0.05)
+            time.sleep(0.02)
+        else:
+            time.sleep(0.1)       
 
 
 
