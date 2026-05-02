d678fa09c152ab7a42e180b19ead996fa71aceeb
HolodTema <art.holodilov@gmail.com>
2026-05-02 07:05:47 -0400

    This code is working, but not in Wayland.

--- trex/main.py
+++ trex/main.py
@@ -3,18 +3,37 @@ import pyautogui
 import pyscreenshot
 import numpy as np
 import keyboard
+import json
+from pathlib import Path
+import matplotlib.pyplot as plt
 
 
-def check_obstacle():
-    BBOX_CHECK_TOP_LEFT_X = 300
-    BBOX_CHECK_TOP_LEFT_Y = 370
-    BBOX_CHECK_BOTTOM_RIGHT_X = 350
-    BBOX_CHECK_BOTTOM_RIGHT_Y = 380
+def import_config_file():
+    path = Path(__file__).parent / "config.json"
+    if not(path.exists()):
+        return None
+    with path.open("r") as f:
+        return json.load(f)
+
+
+
+def check_obstacle(game_window):
+    width = game_window["x_bottom_right"] - game_window["x_top_left"]
+    height = game_window["y_bottom_right"] - game_window["y_top_left"]
+    BBOX_CHECK_TOP_LEFT_X = game_window["x_top_left"] + int(width * 0.15)
+    BBOX_CHECK_TOP_LEFT_Y = game_window["y_bottom_right"] - int(height * 0.3)
+    BBOX_CHECK_BOTTOM_RIGHT_X = game_window["x_top_left"] + int(width * 0.3)
+    BBOX_CHECK_BOTTOM_RIGHT_Y = game_window["y_bottom_right"]
 
     screenshot = pyscreenshot.grab(bbox=(BBOX_CHECK_TOP_LEFT_X, BBOX_CHECK_TOP_LEFT_Y, BBOX_CHECK_BOTTOM_RIGHT_X, BBOX_CHECK_BOTTOM_RIGHT_Y))
     array_screenshot = np.array(screenshot)
-    dark_pixels = np.sum(array_screenshot < 100)
-    return dark_pixels > 20
+    plt.imshow(array_screenshot)
+    plt.show()
+    gray = np.sum(array_screenshot, axis=2)
+    print(gray.shape)
+    dark_pixels = np.sum(gray < 30)
+    print(dark_pixels)
+    return dark_pixels > 5000
 
 
 
@@ -25,6 +44,15 @@ def jump():
     
 def main():
     print("T-REX autorunner (by python_hackershaa)")
+    game_window = import_config_file()
+    if game_window is None:
+        print("Error: you need to create config.json file with coordinates of game window")
+        return
+    
+    print("Game window configured from config.json:")
+    print(f"top_left: ({game_window["x_top_left"]}, {game_window["y_top_left"]})")
+    print(f"bottom_right: ({game_window["x_bottom_right"]}, {game_window["y_bottom_right"]})")
+    print()
     print("1 - start autorun")
     print("2 - stop autorun")
     print("3 - quit")
@@ -48,7 +76,7 @@ def main():
             break
 
         if is_running:
-            if check_obstacle():
+            if check_obstacle(game_window):
                 jump()
                 time.sleep(0.05)
             time.sleep(0.02)
--- trex/selector_overlay.py
+++ trex/selector_overlay.py
@@ -1,22 +1,14 @@
 import subprocess
 import json
-import tkinter as tk
-import sys
 import os
 from pathlib import Path
 
 
 
-def save_config_file(x_top_left, y_top_left, x_bottom_right, y_bottom_right):
+def save_config_file(area):
     path = Path(__file__).parent / "config.json"
-    config = {
-        "x_top_left": x_top_left,
-        "y_top_left": y_top_left,
-        "x_bottom_right": x_bottom_right,
-        "y_bottom_right": y_bottom_right,
-    }
     with path.open("w") as f:
-        json.dump(config, f)
+        json.dump(area, f)
 
 
 
@@ -45,57 +37,11 @@ def select_area_with_slurp():
 
 
 
-def confirmation_dialog(area):
-    root = tk.Tk()
-    root.title("Confirm size of game window")
-    root.geometry("300x300")
-    root.resizable(False, False)
-
-    tk.Label(root, text="Выделена область игры.").pack(pady=5)
-    tk.Label(root, text="Теперь укажите зону, где появляются\nпрепятствия (перед динозавром).").pack(pady=5)
-
-    tk.Label(root, text="Левый X (check_x1):").pack(pady=(10,0))
-    entry_x1 = tk.Entry(root)
-    entry_x1.insert(0, str(area["x_top_left"]))
-    entry_x1.pack()
-
-    tk.Label(root, text="Правый X (check_x2):").pack(pady=(5,0))
-    entry_x2 = tk.Entry(root)
-    entry_x2.insert(0, str(area["x_bottom_right"]))
-    entry_x2.pack()
-
-    tk.Label(root, text="Y уровня земли (check_y):").pack(pady=(5,0))
-    entry_y = tk.Entry(root)
-    entry_y.insert(0, str(area["y_top_left"]))
-    entry_y.pack()
-
-    def on_ok():
-        try:
-            x1 = int(entry_x1.get())
-            x2 = int(entry_x2.get())
-            y = int(entry_y.get())
-            if x1 >= x2:
-                messagebox.showerror("Ошибка", "check_x1 должно быть меньше check_x2")
-                return
-            save_config_file(x1, y, x2, y + 30)
-            root.destroy()
-        except ValueError:
-            messagebox.showerror("Ошибка", "Введите целые числа")
-
-    tk.Button(root, text="✅ Сохранить", command=on_ok).pack(pady=15)
-    root.mainloop()
-
-
-
 def main():
     print("Select game window:")
     area = select_area_with_slurp()
-    if area:
-        confirmation_dialog(area)
-    else:
-        print("Unable to select game window")
-        sys.exit(1)
-
+    save_config_file(area)
+    print("Success! Created file config.json")
 
 
 
